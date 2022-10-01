6ee33a532931d53dd2cfe83a07b712983b0e55b4
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2022-10-01 10:17:48 +0200

    remove quickstart from project

--- quickstart/quickstart-android-annotations/app/src/main/kotlin/org/koin/sample/di/AndroidAppModule.kt
+++
@@ -1,8 +0,0 @@
-package org.koin.sample.di
-
-import org.koin.core.annotation.ComponentScan
-import org.koin.core.annotation.Module
-
-@Module
-@ComponentScan("org.koin.sample")
-class AndroidAppModule
