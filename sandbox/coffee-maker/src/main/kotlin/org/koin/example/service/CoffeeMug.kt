f5a4ae35e1de1b78e8115b10e0c3fc0d6f9239f8
Alperen Babagil <alperen.babagil@1und1.de>
2022-10-10 23:25:28 +0200

    Turned CoffeeMug annotation from Single to Factory. It does not make sense serve everybody with same cup

--- sandbox/coffee-maker/src/main/kotlin/org/koin/example/service/CoffeeMug.kt
+++ sandbox/coffee-maker/src/main/kotlin/org/koin/example/service/CoffeeMug.kt
@@ -2,11 +2,9 @@ package org.koin.example.service
 
 import org.koin.core.annotation.Factory
 import org.koin.core.annotation.Named
-import org.koin.core.annotation.Single
 
-@Single(binds = [Cup::class])
+@Factory(binds = [Cup::class])
 @Named("CoffeeMug")
-
 class CoffeeMug : Cup() {
 
     override fun setMyName(): String = "CoffeeMug"
