2ae4175e91fb5b7c37e7d0c89f8b6294f09fca06
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2024-10-15 17:35:29 +0200

    add sample for it package

--- examples/android-library/src/main/java/org/koin/sample/android/library/CommonModule.kt
+++ examples/android-library/src/main/java/org/koin/sample/android/library/CommonModule.kt
@@ -1,8 +1,9 @@
 package org.koin.sample.android.library
 
+import it.example.component.ExampleModule
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
 
-@Module
+@Module(includes = [ExampleModule::class])
 @ComponentScan
 class CommonModule
