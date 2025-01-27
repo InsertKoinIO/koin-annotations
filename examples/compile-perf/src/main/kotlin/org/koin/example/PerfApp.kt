0185cf0adaa0d5985d7f0633080ddd35ba519563
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2025-01-27 21:45:14 +0100

    split module generation if goes over 500 (#220)

--- examples/compile-perf/src/main/kotlin/org/koin/example/PerfApp.kt
+++ examples/compile-perf/src/main/kotlin/org/koin/example/PerfApp.kt
@@ -1,14 +1,16 @@
 package org.koin.example
 
-import org.koin.core.context.startKoin
+import org.koin.core.context.GlobalContext.startKoin
+import org.koin.example.components.four.MyModule4
 import org.koin.example.components.one.MyModule
 import org.koin.example.components.three.MyModule3
 import org.koin.example.components.two.MyModule2
-
-import org.koin.ksp.generated.*
+import org.koin.ksp.generated.module
+import java.io.File
 
 fun main() {
-//    generate()
+
+//    generateTestFile()
 
     startKoin {
         printLogger()
@@ -16,22 +18,25 @@ fun main() {
 //            defaultModule,
             MyModule().module,
             MyModule2().module,
-            MyModule3().module
+            MyModule3().module,
+            MyModule4().module
         )
     }
 }
 
-//private fun generate() {
-//    for (i in 1..100) {
-//        println(
-//            """
-//    @Single
-//    class ComponentA$i
-//    @Single
-//    class ComponentB$i(val a : ComponentA$i)
-//    @Single
-//    class ComponentC$i(val a : ComponentA$i, val b : ComponentB$i)
-//        """.trimIndent()
-//        )
-//    }
-//}
+private fun generateTestFile(fileName : String = "output.txt") {
+    val content = (1..550).map { i ->
+        """
+    @Single
+    class ComponentAAAA$i
+    @Single
+    class ComponentBBBBB$i(val a : ComponentAAAA$i)
+    @Single
+    class ComponentCCCCC$i(val a : ComponentAAAA$i, val b : ComponentBBBBB$i)
+        """.trimIndent()
+    } .joinToString(separator = "\n")
+
+    val f = File(fileName)
+        f.createNewFile()
+    f.writeText(content)
+}
