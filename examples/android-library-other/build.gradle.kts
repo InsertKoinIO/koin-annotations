00273460425f34babc8358698b2c6edd7f852fdb
Sepehr Akhoundi <Sepehrakhoundi1812@Gmail.com>
2025-02-19 11:41:54 +0330

    Add multi-module classes definition duplication issue case. (#227)
    - Add android-library-other module
    - Add LibFooB class to the new module

--- examples/android-library-other/build.gradle.kts
+++ examples/android-library-other/build.gradle.kts
@@ -1,31 +1,20 @@
 plugins {
-    alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.androidLibrary)
     alias(libs.plugins.ksp)
 }
 
-val androidCompileSDK : String by project
-val androidMinSDK : String by project
+val androidCompileSDK: String by project
+val androidMinSDK: String by project
 
 android {
     compileSdk = androidCompileSDK.toInt()
     defaultConfig {
-        namespace = "org.koin.sample.androidx"
+        namespace = "org.koin.sample.android.library.other"
         minSdk = androidMinSDK.toInt()
-        applicationId = "org.gradle.kotlin.dsl.samples.androidstudio"
-        versionCode = 1
-        versionName = "1.0"
-        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
     }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
-        }
-    }
-
     // to use KSP generated Code
-    applicationVariants.all {
+    libraryVariants.all {
         val variantName = name
         sourceSets {
             getByName("main") {
@@ -48,16 +37,13 @@ dependencies {
     implementation(libs.koin.annotations)
     implementation(libs.android.appcompat)
     ksp(libs.koin.ksp)
-    implementation(project(":android-library"))
-    implementation(project(":android-library-other"))
     implementation(project(":coffee-maker-module"))
-
+    api(libs.ktor.core)
+    implementation(libs.ktor.cio)
     testImplementation(libs.koin.test)
-    testImplementation(libs.junit)
 }
 
 ksp {
-    arg("KOIN_CONFIG_CHECK","true")
-    arg("KOIN_DEFAULT_MODULE","true")
-    arg("KOIN_LOG_TIMES","true")
+    arg("KOIN_CONFIG_CHECK", "true")
+    arg("KOIN_DEFAULT_MODULE", "false")
 }
