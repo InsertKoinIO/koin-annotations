4126299c1070eed47c73b283de3ff3b295bb780f
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2025-07-23 18:19:44 +0200

    add test

--- examples/android-coffee-maker/src/main/java/org/koin/sample/androidx/app/scope/ScopeComponents.kt
+++ examples/android-coffee-maker/src/main/java/org/koin/sample/androidx/app/scope/ScopeComponents.kt
@@ -1,11 +1,9 @@
 package org.koin.sample.androidx.app.scope
 
 import androidx.lifecycle.ViewModel
+import org.koin.android.annotation.ActivityScope
 import org.koin.android.annotation.KoinViewModel
-import org.koin.core.annotation.Scope
-import org.koin.core.annotation.Scoped
 import org.koin.core.annotation.ViewModelScope
-import org.koin.sample.androidx.MainActivity
 import java.util.UUID
 
 @ViewModelScope
@@ -18,6 +16,20 @@ class ScopedOtherData {
     val id = UUID.randomUUID().toString()
 }
 
+@ViewModelScope
+class SecondOtherData(val sd : ScopedData) {
+    val id = sd.id
+}
 
 @KoinViewModel
-class ScopeViewModel(val sd : ScopedData, val sod : ScopedOtherData) : ViewModel()
+class ScopeViewModel(val sd : ScopedData, val sod : ScopedOtherData, val ssd : SecondOtherData) : ViewModel()
+
+@ActivityScope
+class MyActivityScope {
+    val id = UUID.randomUUID().toString()
+}
+
+@ActivityScope
+class MyActivityOtherScope(val mas : MyActivityScope) {
+    val id = mas.id
+}
